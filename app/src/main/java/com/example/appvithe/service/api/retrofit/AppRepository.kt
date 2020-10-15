2ce6d172d0926582223acb9d4c222c2e96b0de52
sivan <sivannguyen1991@gmail.com>
2020-10-15 17:42:42 +0700

    update code

--- app/src/main/java/com/example/appvithe/service/api/retrofit/AppRepository.kt
+++ app/src/main/java/com/example/appvithe/service/api/retrofit/AppRepository.kt
@@ -5,12 +5,20 @@ import com.example.appvithe.model.request.SignUpRequest
 import com.example.appvithe.model.response.BaseResponse
 import io.reactivex.Single
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AppRepository {
-    
+
     @POST("auth/signup")
     fun signup(@Body signUpRequest: SignUpRequest): Single<BaseResponse>
+
     @POST("auth/login")
     fun login(@Body loginRequest: LoginRequest): Single<BaseResponse>
+
+    @GET("auth/logout")
+    fun logout(): Single<BaseResponse>
+
+    @GET("auth/user")
+    fun user(): Single<BaseResponse>
 }
